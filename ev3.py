bd6726bff20631b50c3192dd4dc53b1cce538f1a
vlad <vladbolkunovv@gmail.com>
2019-11-16 23:03:32 +0300

    threads synchronized

--- ev3.py
+++ ev3.py
@@ -1,26 +1,19 @@
+import threading
 import serial
 import time
 from serial import Serial
 
 
 class Ev3:
-    class MsgType:
+    class _MsgType:
         class Mode:
-            first = 0
-            second = 0
+            def __init__(self, first, second):
+                self.second = second
+                self.first = first
 
-        speed = Mode()
-        start = Mode()
-        stop = Mode()
-
-        speed.first = 10
-        speed.second = 165
-
-        start.first = 8
-        start.second = 166
-
-        stop.first = 9
-        stop.second = 163
+        speed = Mode(10, 165)
+        start = Mode(8, 166)
+        stop = Mode(9, 163)
 
     class Motors:
         A = 0b1
@@ -33,15 +26,16 @@ class Ev3:
         BREAK = 1
 
     def __init__(self, port_name):
-        self.ev3 = Serial(port_name, baudrate=9600,
-                          stopbits=serial.STOPBITS_ONE,
-                          parity=serial.PARITY_NONE,
-                          bytesize=serial.EIGHTBITS)
+        self._controller = Serial(port_name, baudrate=9600,
+                                  stopbits=serial.STOPBITS_ONE,
+                                  parity=serial.PARITY_NONE,
+                                  bytesize=serial.EIGHTBITS)
 
         time.sleep(1)
+        self._lock = threading.Lock()
 
     def set_speed(self, motor, val):
-        message = self.pack_msg(self.MsgType.speed)
+        message = self._pack_msg(self._MsgType.speed)
 
         if val < 0:
             val = 256 - abs(val)
@@ -53,20 +47,20 @@ class Ev3:
         message[10] = 129
         message[11] = val
 
-        self.send(message)
+        self._send(message)
 
     def start(self, motor):
-        message = self.pack_msg(self.MsgType.start)
+        message = self._pack_msg(self._MsgType.start)
 
         for i in range(0, 1):
             message.append(0)
 
         message[9] = motor
 
-        self.send(message)
+        self._send(message)
 
     def stop(self, motor, type):
-        message = self.pack_msg(self.MsgType.stop)
+        message = self._pack_msg(self._MsgType.stop)
 
         for i in range(0, 2):
             message.append(0)
@@ -74,15 +68,18 @@ class Ev3:
         message[9] = motor
         message[10] = type
 
-        self.send(message)
+        self._send(message)
 
-    def send(self, message):
-        self.ev3.write(bytes(message))
+    def _send(self, message):
+        try:
+            self._lock.acquire()
+            self._controller.write(bytes(message))
+        finally:
+            self._lock.release()
 
     @staticmethod
-    def pack_msg(mode):
-        msg = [0 for i in range(0, 9)]
-
+    def _pack_msg(mode):
+        msg = [0 for i in range(9)]
         msg[0] = mode.first
         msg[7] = mode.second
         msg[4] = 128
@@ -90,7 +87,7 @@ class Ev3:
         return msg
 
     def close(self):
-        self.ev3.close()
+        self._controller.close()
 
     def __del__(self):
         self.close()
